28d84a6fb5191499f7f75bfb156e594e3f5c43ae
Nathan Minissale <Bitterpilot@users.noreply.github.com>
2017-10-30 19:20:27 +0800

    Video 2

--- app/src/main/java/com/stackskills_learning/android_user_interface_2/MainActivity.java
+++ app/src/main/java/com/stackskills_learning/android_user_interface_2/MainActivity.java
@@ -2,12 +2,39 @@ package com.stackskills_learning.android_user_interface_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = (RadioGroup) findViewById(R.id.RadioGroupID);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                radioButton = (RadioButton) findViewById(checkedId);
+                switch (radioButton.getId()){
+                    case R.id.YesID:{
+                        Log.d("RD","Yes");
+                    }
+                    break;
+                    case R.id.NoID:{
+                        Log.d("RD","no");
+                    }
+                    break;
+                    case R.id.MaybeID:{
+                        Log.d("RD","maybe");
+                    }
+                }
+            }
+        });
     }
 }
