f71c271f651a33097d4a7a9e31c2d0069f18ce39
Nathan Minissale <Bitterpilot@users.noreply.github.com>
2017-10-30 18:44:27 +0800

    Origin

---
+++ app/src/main/java/com/stackskills_learning/android_user_interface_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.stackskills_learning.android_user_interface_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
