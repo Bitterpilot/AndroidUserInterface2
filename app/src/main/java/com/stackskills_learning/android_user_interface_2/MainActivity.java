1fb547e615591dc1aab5f8c35c55e680f052c314
Nathan Minissale <Bitterpilot@users.noreply.github.com>
2017-11-12 17:00:25 +0800

    Video 3

--- app/src/main/java/com/stackskills_learning/android_user_interface_2/MainActivity.java
+++ app/src/main/java/com/stackskills_learning/android_user_interface_2/MainActivity.java
@@ -1,13 +1,45 @@
 package com.stackskills_learning.android_user_interface_2;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTextView = (TextView) findViewById(R.id.resaultID);
+        seekBar = (SeekBar) findViewById(R.id.seekBarID);
+
+        resultTextView.setText("Pain Level! " + seekBar.getProgress() + "/" + seekBar.getMax());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                resultTextView.setTextColor(Color.GRAY);
+                resultTextView.setText("Pain Level! " + seekBar.getProgress() + "/" + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d("SB","onStartTrackingTouch");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (seekBar.getProgress() >= 7){
+                    resultTextView.setTextColor(Color.RED);
+                }
+
+                Log.d("SB","onStopTrackingTouch");
+            }
+        });
+
     }
 }
