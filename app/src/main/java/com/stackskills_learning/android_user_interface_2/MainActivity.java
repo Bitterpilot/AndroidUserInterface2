02447c6c996b631a55b280a0d8cb3fb02a2182eb
Bitterpilot <Bitterpilot@users.noreply.github.com>
2017-11-13 16:30:15 +0800

    Video 4

--- app/src/main/java/com/stackskills_learning/android_user_interface_2/MainActivity.java
+++ app/src/main/java/com/stackskills_learning/android_user_interface_2/MainActivity.java
@@ -2,12 +2,33 @@ package com.stackskills_learning.android_user_interface_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ToggleButton toggleButton;
+    private TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultView = (TextView) findViewById(R.id.peekabooTextView);
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButtonID);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    //Toggle is enabled
+                    resultView.setVisibility(View.VISIBLE);
+                }else{
+                    //Toggle is disabled
+                    resultView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
